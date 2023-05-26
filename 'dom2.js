700ce75bf340d5a1056087d04cdb5c4e3716de4a
nayaknainshi <pathaknainshisatyam@gmail.com>
2023-05-26 09:12:30 +0530

    first commit

---
+++ 'dom2.js
@@ -0,0 +1,20 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+
+
+
+
+//var li =document.getElementsByTagName('list-group-item');
+//console.log(item);
+//console.log(item[1]);
+//item[1].textContent = 'hello2';
+//item[1].style.fontweight = 'bold';
+//item[1].style.background = yellow;
+//for(var i = 0; i<item.lenght; i++){
+  //  item[i].style.backgroundColor = '#f4f4f4';
+//}
+
+//var header = document.querySelector('#main-header');
+//header.getElementsByClassName.borderBottom = 'solid 4px #ccc';
+
