eee0459d1786ee349184bb800c28652d8bb56ce6
nayaknainshi <134363859+nayaknainshi@users.noreply.github.com>
2023-06-13 15:45:56 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,7 @@
+<script>
+            function onsubmit(event) {
+                const Name = document.getElementById('Name').value;
+                const phone = document.getElementById('phone').value;
+                console.log(Name)
+            }                   
+        </script>
